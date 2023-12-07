78841418bcd52cf7360e02a6ceb43c8c8fa03570
sriaman <amansrivastav.cse20@jecrc.ac.in>
2023-12-07 21:23:13 +0530

    Create and call internally CurrencyExchange API

--- src/main/java/com/java/ms/asyncprogramming/controller/CurrencyExchangeController.java
+++ src/main/java/com/java/ms/asyncprogramming/controller/CurrencyExchangeController.java
@@ -1,13 +1,10 @@
 package com.java.ms.asyncprogramming.controller;
 
 import com.java.ms.asyncprogramming.model.Currency;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.java.ms.asyncprogramming.model.Currency.init;
 
 @RestController
 @RequestMapping("/api/async")
